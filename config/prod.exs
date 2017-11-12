7878109f9087cf290b924b6b8100a1dcbec574ca
TuukkaP <tuukka.peuraniemi@gmail.com>
2017-11-12 13:25:22 +0200

    Bump elm-brunch to 0.10 + cache-manifest.json fix

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :lukimat, LukimatWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "mappula.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
