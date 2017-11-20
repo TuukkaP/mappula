1626a4f2e36989da3e3a417d7e9b6519d79e81e4
TuukkaP <tuukka.peuraniemi@gmail.com>
2017-11-20 20:49:32 +0200

    Override default S3 region

--- config/prod.exs
+++ config/prod.exs
@@ -45,7 +45,8 @@ config :arc,
 
 config :ex_aws,
   access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
-  secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY")
+  secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
+  region: "eu-central-1"
 #
 #
 # To get SSL working, you will need to add the `https` key
